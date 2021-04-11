961a424a9e08b9b49923bccb64d445ac21feece7
Murat ÇATMALI <catmalimurat@gmail.com>
2021-04-11 21:40:44 +0300

    Add project files.

---
+++ kayit.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class bireysel : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+     
+        if (DropDownList2.SelectedIndex>=0)
+        {
+            
+            //form bilgilerini gönderiyoruz...
+
+                Response.Redirect("onay.aspx?adtxt=" + adtxt.Text + "&soyadtxt=" + soyadtxt.Text + "&emailtxt=" + emailtxt.Text + "&tctxt=" + tctxt.Text + "&bolum=" + DropDownList2.SelectedValue + "&sinif=" +  "&sfr="+sfrtxt.Text);
+                        //Response.Redirect("kayit.aspx?adtxt="+ adtxt.Text+"&emailtxt="+emailtxt.Text+"&teltxt="+teltxt.Text+"&cinsiyet="+cinsiyet+"&kurs="+DropDownList3.SelectedValue + "&onay="+onayli+"&isdurumu="+isdurumu+"&sehir="+ DropDownList1.SelectedValue+"&ozeltxt="+ozeltxt.Text);            
+            
+        }
+        else
+        {
+            Response.Write("<script>alert('Bölüm Seçmelisiniz')</script>");
+        }
+    }
+}
---
+++ onay.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class kayit : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //öğrenci kaydı
+
+        string ad = Request.QueryString["adtxt"];
+        string soyad = Request.QueryString["soyadtxt"];
+        string email = Request.QueryString["emailtxt"];
+        string tc = Request.QueryString["tctxt"];
+        string sfr = Request.QueryString["sfr"];
+        string bolum = Request.QueryString["bolum"];     
+        
+        
+
+        string tarih = System.DateTime.Now.ToShortTimeString();
+        DbCRUD dbcrud = new DbCRUD();
+        dbcrud.baglanti.Open();
+        
+        SqlCommand komut = new SqlCommand("INSERT INTO TblOgrenciler (O_Tc_Kimlik,O_Ad,O_Soyad,O_Sifre,[O_E-mail],O_Bolum) values (@p1,@p2,@p3,@p4,@p5,@p6)", dbcrud.baglanti);
+        komut.Parameters.AddWithValue("@p1", tc);
+        komut.Parameters.AddWithValue("@p2", ad);
+        komut.Parameters.AddWithValue("@p3", soyad);
+        komut.Parameters.AddWithValue("@p4", sfr);
+        komut.Parameters.AddWithValue("@p5", email);
+        komut.Parameters.AddWithValue("@p6", bolum);
+     
+        komut.ExecuteNonQuery();
+        dbcrud.baglanti.Close();
+       
+        sonuc.InnerHtml = "Uzaktan Eğitim Sistemine Kaydınız Yapılmıştır. Cv nizi düzenlemek için Tıklayınız";
+
+    
+		 
+	}
+}
